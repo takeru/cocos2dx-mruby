c77b6cb6e6cd4e9fc82cc0fbf1a483560f857c4b
Keita Obo <ktaobo@gmail.com>
2013-09-18 15:13:13 +0900

    Remove comment outed code

--- example/Classes/AppDelegate.cpp
+++ example/Classes/AppDelegate.cpp
@@ -2,8 +2,8 @@
 //  CocosMrubyAppDelegate.cpp
 //  CocosMruby
 //
-//  Created by admin on 13/09/17.
-//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//  Created by ktaobo on 13/09/17.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
 //
 
 #include "AppDelegate.h"
@@ -17,7 +17,6 @@ using namespace CocosDenshion;
 
 AppDelegate::AppDelegate()
 {
-
 }
 
 AppDelegate::~AppDelegate()
@@ -26,25 +25,6 @@ AppDelegate::~AppDelegate()
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
-#if 0
-    // initialize director
-    CCDirector *pDirector = CCDirector::sharedDirector();
-    pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
-
-    // turn on display FPS
-    pDirector->setDisplayStats(true);
-
-    // set FPS. the default value is 1.0/60 if you don't call this
-    pDirector->setAnimationInterval(1.0 / 60);
-
-    // create a scene. it's an autorelease object
-    CCScene *pScene = HelloWorld::scene();
-
-    // run
-    pDirector->runWithScene(pScene);
-
-    return true;
-#else
   // initialize director
   CCDirector *pDirector = CCDirector::sharedDirector();
   pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
@@ -55,7 +35,7 @@ bool AppDelegate::applicationDidFinishLaunching()
   // set FPS. the default value is 1.0/60 if you don't call this
   pDirector->setAnimationInterval(1.0 / 60);
   
-  // register lua engine
+  // register mruby engine
   CCMrubyEngine* pEngine = CCMrubyEngine::defaultEngine();
   CCScriptEngineManager::sharedManager()->setScriptEngine(pEngine);
   
@@ -63,7 +43,6 @@ bool AppDelegate::applicationDidFinishLaunching()
   pEngine->executeScriptFile(path.c_str());
   
   return true;
-#endif
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
